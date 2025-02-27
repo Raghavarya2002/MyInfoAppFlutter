b8bdf8283cb458baccb5e084af40780de7593690
raghav41 <raghav@trulyfreehome.com>
2025-02-27 19:00:45 +0530

    Developed My Info App in Flutter

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'All About Raghav Arya',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontFamily: 'Pacifico', fontSize: 20.0, color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/68361729?v=4')),
+              Text(
+                'Raghav Arya',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Code Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+91 9254579976',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'Source Code Pro',
+                        fontSize: 16.0),
+                  ),
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'raghavarya2002@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 16.0),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
